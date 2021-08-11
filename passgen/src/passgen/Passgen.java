58e8d7f51875094d1c4726da2bd61e4391cd226b
Rajesh Kumar <48724570+rajeshratann@users.noreply.github.com>
2021-08-12 00:58:48 +0530

    update file
    update file

---
+++ passgen/src/passgen/Passgen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passgen;
+
+
+/**
+ *
+ * @author rajesh
+ */
+public class Passgen {
+
+    public static void main(String[] args) {
+       PassgenFrame frame = new PassgenFrame();
+       frame.show();
+  }
+}
